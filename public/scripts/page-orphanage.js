5ddf45edd9b806d84e00ec3653582e1808b85086
Victor Junior <victorjr.muchacho@gmail.com>
2020-10-14 15:02:21 -0300

    Day 2 3/3

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,54 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-15.8329381, -48.0699353], 15);
+
+// create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl:"./public/images/map-marker.svg",
+  iconSIze: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
+
+// create and add marker
+L
+.marker([-15.8329381, -48.0699353], { icon })
+.addTo(map);
+
+/* image gallery */
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  // remove all classes active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  } 
+  /* alternativa com arrowFunction
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  */
+
+  // select image clicked
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  // actualize the container of image
+  imageContainer.src = image.src;
+
+  // add the class active for button
+  button.classList.add("active");
+}
