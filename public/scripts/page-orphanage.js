25b216c291fe40425da33f9c4575e52bab4f5005
Victor Junior <victorjr.muchacho@gmail.com>
2020-10-25 23:00:04 -0300

    Day 5 3/4

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,12 @@ const options = {
   zoomControl: false
 }
 
+// get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat;
+const lng = document.querySelector('span[data-lng]').dataset.lng;
+
 // create map
-const map = L.map('mapid', options).setView([-15.8329381, -48.0699353], 15);
+const map = L.map('mapid', options).setView([lat, lng], 15);
 
 // create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -22,7 +26,7 @@ const icon = L.icon({
 
 // create and add marker
 L
-.marker([-15.8329381, -48.0699353], { icon })
+.marker([lat, lng], { icon })
 .addTo(map);
 
 /* image gallery */
---
+++ src/database/test.js
@@ -0,0 +1,32 @@
+const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
+
+Database.then(async db => {
+  // inserir dados na tabela
+  await saveOrphanage(db, {
+    lat: "-15.8329381",
+    lng: "-48.0799353",
+    name: "Lar dos Meninos",
+    about: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    whatsapp: "98989897898",
+    images: [
+      "https://images.unsplash.com/photo-1595295403848-3f1d04c4e95e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1598749953342-b4ee75629dca?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ].toString(),
+    instructions: "Venha como se sentir à vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "0"
+  });
+
+  // consultar dados da tabela
+  const selectedOrphanages = await db.all("SELECT * FROM orphanages");
+  console.log(selectedOrphanages);
+
+  // consultar somente 1 orfanato, pelo id
+  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"');
+  console.log(orphanage);
+
+  // deletar dado da tabela
+  // console.log(await db.run('DELETE FROM orphanages WHERE id = "4"'));
+});
